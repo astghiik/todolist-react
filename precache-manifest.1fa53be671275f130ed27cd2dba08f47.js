98b0ad541bbe4eb69d2a468d5e8955d80991649e
Astghik Khachatryan <astghikhachatrian@gmail.com>
2019-01-25 01:27:14 +0400

    Updates

---
+++ precache-manifest.1fa53be671275f130ed27cd2dba08f47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39cff1a79defb0fe09c",
+    "url": "/todolist-react/static/js/runtime~main.d39cff1a.js"
+  },
+  {
+    "revision": "69d557b9d78a456e903a",
+    "url": "/todolist-react/static/js/main.69d557b9.chunk.js"
+  },
+  {
+    "revision": "1b9f2f8850ae9e543f20",
+    "url": "/todolist-react/static/js/1.1b9f2f88.chunk.js"
+  },
+  {
+    "revision": "69d557b9d78a456e903a",
+    "url": "/todolist-react/static/css/main.ab1435c8.chunk.css"
+  },
+  {
+    "revision": "304e30e8e922b1e730a0d8aad4a3ffac",
+    "url": "/todolist-react/index.html"
+  }
+];
